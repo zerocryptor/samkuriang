6740637f80466573a752fa11ce8cba6d0d4d789e
dzakifa <39705164+dzakifa@users.noreply.github.com>
2019-05-16 12:06:50 +0700

    dzaki #5 (update)
    - update seeder (update)

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -14,16 +14,6 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        $faker = Faker::create('id_ID');
-        for ($i=0; $i<5 ; $i++) {
-            DB::table ('admins')->insert([
-                'username' => $faker->userName,
-                'password' => Hash::make('admin'),
-                'db_user' => 1
-            ]);
-        }
-=======
         // $faker = Faker::create('id_ID');
         // for ($i=0; $i<5 ; $i++) {
         //     DB::table ('AdminsTableSeeder')->insert([
@@ -40,6 +30,5 @@ class AdminsTableSeeder extends Seeder
             'address' => 'Jalan Admin No. 12',
             'phone_number' => '08957271637'
         ]);
->>>>>>> e8093d2317749452d5d1720d12273eb8efb2a890
     }
 }
