497ecf693484cdd1aaf91e66b678793cab486a0a
dzakifa <39705164+dzakifa@users.noreply.github.com>
2019-05-27 13:33:58 +0700

    r
    r

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,49 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
-        .logo {height: 70px; width: 70px;}
-        .navbar-light{
-            background-color: #e8f8eb;
-        }
-
-        .navbar-brand{
-            color: #055a3d;
-        }
-
-        #topheader .navbar-nav li > a {
-            text-transform: capitalize;
-            color: #333;
-            transition: color .2s;
-        }
-        #topheader .navbar-nav li > a:hover, #topheader .navbar-nav li > a:focus {
-            color: #31a05e;
-        }
-        #topheader .navbar-nav li.active > a {
-            color: #31a05e;
-        }
-
-        .btn {
-            width: 110px;
-            text-align: center;
-            transition: 0.7s;
-            background-size: 180% auto;
-            color: white;
-            box-shadow: 0 0 20px #eee;
-            border-radius: 40px;
-        }
-        .btn:hover {
-            background-position: right center;
-            color: white;
-        }
-        .btn-1 {
-            background-image: linear-gradient(to right, #055a3d 0%, #002031 51%, #055a3d 100%);
-        }
-    </style>
 </head>
 <body>
     <nav id="topheader" class="navbar navbar-expand-lg navbar-light bg-light pt-3">
-        <a class="navbar-brand pl-3 mb-3 pt-4" href="#"><img src="../../public/asset/logo.png" alt="" class="logo"><strong>Samkuriang</strong></a>
+        <a class="navbar-brand pl-3 mb-3 pt-4" href="#"><img src="public/asset/logo.png" alt="" class="logo"><strong>Samkuriang</strong></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>Samkuriang | A fine glass vase goes from treasure to trash, the moment it is broken.</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,65 +16,46 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('vendors/bootstrap-4.0.0/dist/css/bootstrap.min.css') }}">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <nav id="topheader" class="navbar navbar-expand-lg navbar-light bg-light pt-3">
+        <a class="navbar-brand pl-3 mb-3 pt-4" href="#"><img src="../../public/asset/logo.png {{ asset('vendors/bootstrap-4.0.0/dist/css/bootstrap.min.css') }}" alt="" class="logo"><strong>Samkuriang</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        <li class="nav-item dropdown">
-
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <div class="collapse navbar-collapse pr-4" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto pr-3">
+                <li class="nav-item active pr-3">
+                    <a class="nav-link" href="#">Beranda<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item pr-3">
+                    <a class="nav-link" href="#">Fitur</a>
+                </li>
+                <li class="nav-item pr-3">
+                    <a class="nav-link" href="#">Mitra</a>
+                </li>
+                <li class="nav-item pr-3">
+                    <a class="nav-link" href="#">FAQ</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0 btn-1" type="submit">Masuk</button>
+            </form>
+        </div>
+    </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <!-- script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
