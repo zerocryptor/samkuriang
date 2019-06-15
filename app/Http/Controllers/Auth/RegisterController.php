45ecdc452e36aa782273e50c38feec2400e22ed3
zerocryptor <zerocryptor@users.noreply.github.com>
2019-06-15 21:38:55 +0700

    custom style, add regis for garbage officer

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use App\GarbageOfficer;
+use App\Models\User;
+use App\Models\GarbageOfficer;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Http\Request;
+use App\Notifications\NewGarbageOfficer;
+// use Notification;
+
 class RegisterController extends Controller
 {
     /*
@@ -39,8 +41,6 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
-        $this->middleware('guest:user')->except('logout');
-        $this->middleware('guest:garbage_officer')->except('logout');
     }
 
     /**
@@ -53,8 +53,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:garbage_officers'],
+            'password' => ['required', 'string', 'min:6', 'confirmed']
         ]);
     }
 
@@ -62,20 +62,14 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\GarbageOfficer
+     * @return \App\Models\GarbageOfficer
      */
-    protected function createGarbageOfficer(array $data)
+    protected function create(array $data)
     {
         return GarbageOfficer::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
         ]);
-        return redirect()->intended('login/garbage_officer');
-    }
-
-    public function showGarbageOfficerRegisterForm()
-    {
-        return view('auth.register', ['url' => 'garbage_officer ']);
     }
 }
