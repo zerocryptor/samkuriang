c3b21c62c66102b800ccbb109e729bc40e53c06d
andiauu <39704370+andiauu@users.noreply.github.com>
2019-06-18 21:32:40 +0700

    mask nomor

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -253,6 +253,12 @@
 
     </script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/igorescobar/jQuery-Mask-Plugin/1ef022ab/dist/jquery.mask.min.js"></script>
+    <script>
+         $( '#phone_number' ).mask('0000−0000−0000');
+    </script>
+
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
