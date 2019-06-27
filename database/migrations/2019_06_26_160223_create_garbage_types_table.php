ca993c1434c5adcfe30ebe3058063db4a5c8705a
dimasrmp <dimasramaputra@gmail,com>
2019-06-27 08:02:16 +0700

    merge garbage types

---
+++ database/migrations/2019_06_26_160223_create_garbage_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGarbageTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('garbage_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type',25)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('garbage_types');
+    }
+}
